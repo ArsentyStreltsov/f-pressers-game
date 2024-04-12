a9601a24d150ef8dc489fa9642b15074936fade9
ArsentyStreltsov <arsentystreltsov@gmail.com>
2024-04-12 03:54:23 +0200

    finalshot addiding

--- second_try/config-unity.js
+++ second_try/config-unity.js
@@ -11,41 +11,43 @@ let config = {
 };
 
 let elemCanvas = document.querySelector("#unity-canvas");
-
-// used by Unity
-var SetLoadProgress = function(progress)
-{
-	// console.log(`[ SetLoadProgress ] ${progress}`);
-	
+var SetLoadProgress = function (progress) {
 	let remapped = 0.0;
-	if (progress <= 1.0)
-	{
+	if (progress <= 1.0) {
 		remapped = 0.75 * progress;
-	}
-	else
-	{
+	} else {
 		remapped = 0.75 + 0.25 * (progress - 1.0);
 	}
-
 	let elemProgressFull = document.querySelector("#unity-progress-bar-full");
 	elemProgressFull.style.width = `${100 * remapped}%`;
 };
 
-var NotifyLoaded = function()
-{
-	// console.log("[ NotifyLoaded ]");
-
+var NotifyLoaded = function () {
 	let elemCover = document.querySelector("#loading-cover");
-	elemCover.style.display = "none";
+	let movingLogo = document.getElementById('moving-logo');
+	let finalShotVideo = document.getElementById('final-shot-video');
+
+	movingLogo.style.display = 'none';
+	finalShotVideo.style.display = 'block';
+
+	finalShotVideo.onplay = function () {
+		setTimeout(function () {
+			finalShotVideo.classList.add('expanded');
+		}, 500); 
+	};
+
+	finalShotVideo.onended = function () {
+		elemCover.style.display = 'none';
+	};
 };
+
 
 let script = document.createElement("script");
 script.src = loaderUrl;
 script.onload = () => {
 	createUnityInstance(elemCanvas, config, SetLoadProgress)
-	.catch(message =>
-	{
-		alert(message);
-	});
+		.catch(message => {
+			alert(message);
+		});
 };
 document.body.appendChild(script);
